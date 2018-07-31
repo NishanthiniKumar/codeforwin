8ca793aca25b6d671a7bef838f55a9e88cea318b
NishanthiniKumar <41899378+NishanthiniKumar@users.noreply.github.com>
2018-07-31 20:48:48 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+char ch;
+scanf("%c",&ch);
+if(ch>='a' && ch<='z')
+  {
+    if(ch=='a' || ch=='e'||ch=='i'||ch=='o'||ch=='u')
+      printf("vowel");
+    else
+      printf("consonant");
+   }
+   else
+    printf("invalid");
+   return 0;
+   }
